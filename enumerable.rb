e490fb0c7d685bd8641cd1c82827e034d6038436
Leonard Soai-Van <leo.soaivan@gmail.com>
2016-07-13 21:50:24 -0400

    Commit of Enumerable methods (count-map2)

--- enumerable.rb
+++ enumerable.rb
@@ -65,4 +65,55 @@ module Enumerable
         status
     end
 
+    def my_count (item = NIL)
+        count = 0
+        for i in self
+            if block_given?
+                count += 1 if yield(i)
+            elsif item != NIL
+                count += 1 if i == item
+            else
+                count += 1
+            end
+        end
+        count
+    end
+
+    def my_map
+        new_ary = []
+        if block_given?
+            for i in self
+                new_ary << yield(i)
+            end
+        else return enum_for(:my_map)
+        end
+        return new_ary
+    end
+
+    def my_inject(init = NIL)
+        ary = self.to_a
+        if init.nil?
+            init = self.first
+            ary[1..-1].my_each { |i| init = yield(init, i) }
+        else
+            ary.my_each { |i| init = yield(init, i) }
+        end
+        init
+    end
+
+    def my_map2(proc = NIL)
+        new_ary = []
+        if (proc || block_given?)
+            self.my_each do |i|
+                new_ary << ( proc ? proc.call(i) : yield(i) )
+                end
+                    else return enum_for(:my_map2)
+                    end
+                return new_ary
+            end
+
+end
+
+def multiply_els(ary)
+    return ary.my_inject { |product, i| product * i }
 end
